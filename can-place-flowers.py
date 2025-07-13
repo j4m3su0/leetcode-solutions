9a03442b180c490714810a2b9319af5c8d1df953
James Onyegbosi <166926205+j4m3su0@users.noreply.github.com>
2025-07-13 19:58:08 +0100

    Update can-place-flowers.py

--- can-place-flowers.py
+++ can-place-flowers.py
@@ -8,13 +8,13 @@ class Solution:
 
         for i in range(len(flowerbed)):
             if flowerbed[i] == 0 and n > 0:
-                if i == 0 and flowerbed[i+1] == 0:
+                if i == 0 and flowerbed[i + 1] == 0:
                     flowerbed[i] = 1
                     n -= 1
-                elif i == len(flowerbed) - 1 and flowerbed[i-1] == 0:
+                elif i == len(flowerbed) - 1 and flowerbed[i - 1] == 0:
                     flowerbed [i] = 1
                     n -= 1
-                elif flowerbed[i-1] == 0 and flowerbed[i+1] == 0:
+                elif flowerbed[i - 1] == 0 and flowerbed[i + 1] == 0:
                     flowerbed[i] = 1
                     n -= 1
 
